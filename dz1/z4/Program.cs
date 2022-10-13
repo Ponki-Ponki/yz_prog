b35df3a041f941361ec7f10e0a77ed74d7559fd6
Ponki-Ponki <vensop@gmail.com>
2022-10-13 22:07:31 +0300

    готовое дз

---
+++ dz1/z4/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+Console.Write("Введите 3 числа: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (a>b && a>c) Console.Write($" max = {a}");
+if (b>a && b>c) Console.Write($" max = {b}");
+if (c>a && c>b) Console.Write($" max = {c}");
