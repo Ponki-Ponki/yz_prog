dc553c7cc765ad4e11292f8b67663fd316766d7b
Ponki-Ponki <vensop@gmail.com>
2022-11-07 21:10:01 +0300

    update dz4

--- dz4/z27/Program.cs
+++ dz4/z27/Program.cs
@@ -2,3 +2,18 @@
 // 452 -> 11
 // 82 -> 10
 // 9012 -> 12
+
+
+void SumNumbers(int a){
+    int sumNam = 0;
+    a = Math.Abs(a);
+    while (a > 0)
+    {
+        sumNam += a % 10;
+        a /= 10;
+    };
+    Console.WriteLine($"Сумма цифр числа равна: {sumNam}");
+}
+
+ Console.Write("Введите число: ");
+ SumNumbers(Convert.ToInt32(Console.ReadLine()));
