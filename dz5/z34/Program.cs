fd88239eafab6b49e122b6f6bcae584aa776ce03
Ponki-Ponki <vensop@gmail.com>
2022-11-12 14:32:16 +0300

    update dz5

---
+++ dz5/z34/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int[] CreateArray(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(min, max);
+    }
+    return array;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i != arr.Length - 1) Console.Write(arr[i] + ", ");
+        else Console.Write(arr[i]);
+    }
+    Console.Write("]");
+}
+
+int SumEvenNum(int[] arr)
+{   
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i]% 2 ==0) count++;
+    }
+    return count;
+}
+
+void PrintResult(int result)
+{
+    Console.WriteLine($" -> {result}");
+    Console.WriteLine($"Количество чётных чисел в массиве = {result}");
+}
+
+
+int[] arr = CreateArray(10, 100, 1000);
+PrintArray(arr);
+int result = SumEvenNum(arr);
+PrintResult(result);
