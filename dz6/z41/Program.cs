7576237b853338cd59613ac6fad6dd0689edddfe
Ponki-Ponki <vensop@gmail.com>
2022-11-15 15:37:01 +0300

    update

---
+++ dz6/z41/Program.cs
@@ -0,0 +1,29 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+int[] CreateArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+void CalckNum(int[] arr)
+{
+    int res = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0) res++;
+    }
+    Console.Write($"Чисел больше 0 :  {res}");
+}
+
+
+Console.Write("Сколько вы введёте чисел?");
+int mNum = Convert.ToInt32(Console.ReadLine());
+int[] arr = CreateArray(mNum);
+CalckNum(arr);
---
+++ dz6/z43/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+// y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+void PointCalck(double b1, double k1, double b2, double k2)
+{
+    if (k1 == k2) Console.WriteLine("Две линии паралельны");
+    else {
+        double x = (b2 - b1) / (k1 - k2);
+        double y = k1 * x + b1;
+        Console.WriteLine($"({x};{y})");
+    }
+}
+
+
+Console.Write("Введите координату 1 линии 1ой точки :");
+int b1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату 1 линии 2ой точки :");
+int k1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату 2 линии 1ой точки :");
+int b2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату 2 линии 2ой точки :");
+int k2 = Convert.ToInt32(Console.ReadLine());
+PointCalck(b1, k1, b2, k2);
+// PointCalck(2, 5, 4, 9);
