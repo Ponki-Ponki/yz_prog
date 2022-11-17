ea10abf73f1f6bb9bf03a3449d1dc3178ff77020
Ponki-Ponki <vensop@gmail.com>
2022-11-17 12:42:40 +0300

    update

--- dz6/z43/Program.cs
+++ dz6/z43/Program.cs
@@ -2,14 +2,19 @@
 // y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
-void PointCalck(double b1, double k1, double b2, double k2)
+string PointCalck(double b1, double k1, double b2, double k2)
 {
-    if (k1 == k2) Console.WriteLine("Две линии паралельны");
+    if (k1 == k2) return "Две линии паралельны";
     else {
         double x = (b2 - b1) / (k1 - k2);
         double y = k1 * x + b1;
-        Console.WriteLine($"({x};{y})");
+        return $"({x};{y})";
     }
+}
+
+void PrintResult(string result)
+{
+    Console.WriteLine(result.Length < 12 ? $"Точка пересечения: {result}" : result);
 }
 
 
@@ -21,5 +26,6 @@ Console.Write("Введите координату 2 линии 1ой точки
 int b2 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите координату 2 линии 2ой точки :");
 int k2 = Convert.ToInt32(Console.ReadLine());
-PointCalck(b1, k1, b2, k2);
-// PointCalck(2, 5, 4, 9);
+PrintResult(PointCalck(b1, k1, b2, k2));
+
+// PrintResult(PointCalck(2, 5, 4, 9));
